d554cd4f9f2ec1b965bf3aef9fb27aaf1759339b
eric88525 <eric885251@gmail.com>
2020-08-12 00:25:51 +0800

    add DB & table

--- itWeb/Controllers/HomeController.cs
+++ itWeb/Controllers/HomeController.cs
@@ -6,13 +6,57 @@ using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
-
+// db
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace itWeb.Controllers
 {
     public class HomeController : Controller
     {
-        
+        string connString = "server=127.0.0.1;port=3306;user id=mvcuser;password=mvcpassword;database=mvc;charset=utf8;";
+        MySqlConnection conn = new MySqlConnection();
+
+        public ActionResult Create()
+        {
+            conn.ConnectionString = connString;
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
+            string sql = @"INSERT INTO `City` (`Id`, `City`) VALUES
+                           ('0', '基隆市'),
+                           ('1', '臺北市'),
+                           ('2', '新北市'),
+                           ('3', '桃園市'),
+                           ('4', '新竹市'),
+                           ('5', '新竹縣'),
+                           ('6', '宜蘭縣'),
+                           ('7', '苗栗縣'),
+                           ('8', '臺中市'),
+                           ('9', '彰化縣'),
+                           ('A', '南投縣'),
+                           ('B', '雲林縣'),
+                           ('C', '嘉義市'),
+                           ('D', '嘉義縣'),
+                           ('E', '臺南市'),
+                           ('F', '高雄市'),
+                           ('G', '屏東縣'),
+                           ('H', '澎湖縣'),
+                           ('I', '花蓮縣'),
+                           ('J', '臺東縣'),
+                           ('K', '金門縣'),
+                           ('L', '連江縣');";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            int index = cmd.ExecuteNonQuery();
+            bool success = false;
+            if (index > 0)
+                success = true;
+            else
+                success = false;
+            ViewBag.Success = success;
+            conn.Close();
+            return View();
+
+        }
 
         public ActionResult Index()
         {
@@ -41,6 +85,7 @@ namespace itWeb.Controllers
             return View(data);
         }
 
+        /* this is post by FormCollection
         [HttpPost]
         public ActionResult Transcripts(FormCollection post)
         {
@@ -49,10 +94,18 @@ namespace itWeb.Controllers
             int score = Convert.ToInt32(post["score"]);
             Student data = new Student(id,name,score);
             return View(data);
-
-
         }
+        */
 
+        [HttpPost]
+        public ActionResult Transcripts(Student model)
+        {
+            string id = model.id;
+            string name = model.name;
+            int score = model.score;
+            Student data = new Student(id,name,score);
+            return View(data);
+        }
 
 
     }
