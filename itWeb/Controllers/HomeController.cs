aeec34814592c5c0f82618d93575b5ef96ff251e
eric88525 <eric885251@gmail.com>
2020-08-11 22:37:03 +0800

    1. build a form 2. form get method 3. add Home/Transcript to show the form data

--- itWeb/Controllers/HomeController.cs
+++ itWeb/Controllers/HomeController.cs
@@ -1,62 +1,45 @@
-﻿using System;
+﻿
+using itWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace itWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public class Student
-        {
-            public string id { get; set; }
-            public string name { get; set; }
-            public int score { get; set; }
-
-            public Student()
-            {
-                id = string.Empty;
-                name = string.Empty;
-                score = 0;
-            }
-
-            public Student(string _id, string _name, int _score)
-            {
-                id = _id;
-                name = _name;
-                score = _score;
-            }
-
-            public override string ToString()
-            {
-                return $"學號: {id} ,Name: {name} , score:  {score}";
-            }
-
-        }
-
-
-
+        
 
         public ActionResult Index()
         {
             DateTime date = DateTime.Now;
-            Student data = new Student();
+            Student data = new Student("", "", 0);
+            
             List<Student> list = new List<Student>();
             list.Add(new Student("1","ERIC",80));
             list.Add(new Student("2", "JOHN", 30));
             list.Add(new Student("3", "LUCKY", 40));
             list.Add(new Student("4", "JOHN", 20));
             list.Add(new Student("5", "TRACY", 10));
-
+            
             ViewBag.Date = date;
             ViewBag.Student = data;
             ViewBag.List = list;
 
 
-            return View();
+            return View(data);
         }
+        public ActionResult Transcripts(string id,string name , int score )
+        {
+            Student data = new Student(id, name, score);
 
+
+            return View(data);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ itWeb/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace itWeb.Models
+{
+    public class Student
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public int score { get; set; }
+
+        public Student()
+        {
+            id = string.Empty;
+            name = string.Empty;
+            score = 0;
+        }
+
+        public Student(string _id, string _name, int _score)
+        {
+            id = _id;
+            name = _name;
+            score = _score;
+        }
+
+        public override string ToString()
+        {
+            return $"學號: {id} ,Name: {name} , score:  {score}";
+        }
+
+    }
+}
