b8a609d11098b9e9b81573f4c9f5e3eccbc382d1
eric88525 <eric885251@gmail.com>
2020-08-11 21:12:24 +0800

    learn ViewBag 01

--- itWeb/Controllers/HomeController.cs
+++ itWeb/Controllers/HomeController.cs
@@ -8,8 +8,52 @@ namespace itWeb.Controllers
 {
     public class HomeController : Controller
     {
+        public class Student
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public int score { get; set; }
+
+            public Student()
+            {
+                id = string.Empty;
+                name = string.Empty;
+                score = 0;
+            }
+
+            public Student(string _id, string _name, int _score)
+            {
+                id = _id;
+                name = _name;
+                score = _score;
+            }
+
+            public override string ToString()
+            {
+                return $"學號: {id} ,Name: {name} , score:  {score}";
+            }
+
+        }
+
+
+
+
         public ActionResult Index()
         {
+            DateTime date = DateTime.Now;
+            Student data = new Student();
+            List<Student> list = new List<Student>();
+            list.Add(new Student("1","ERIC",80));
+            list.Add(new Student("2", "JOHN", 30));
+            list.Add(new Student("3", "LUCKY", 40));
+            list.Add(new Student("4", "JOHN", 20));
+            list.Add(new Student("5", "TRACY", 10));
+
+            ViewBag.Date = date;
+            ViewBag.Student = data;
+            ViewBag.List = list;
+
+
             return View();
         }
 
